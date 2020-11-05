c763253fe8a76627daf1913bd93297cf61e3c8a4
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2020-11-04 20:31:15 -0600

    DBConnection header ready

---
+++ Database/connTest.cpp
@@ -0,0 +1,26 @@
+//NOTE:
+//Must have appropriate drives in your complier settings
+//Must download "Connecter/C" From MySql website
+//connectionDB.h is hidden for security purpopese.
+//Make your own connectionDB.h that with three function that return you DB link, DB username, DB password.
+//Must complie this file with -lmysqlcppconn
+
+#include <iostream>
+#include <stdlib.h>
+
+#include <mysql/include/jdbc/mysql_connection.h>
+#include <mysql/include/jdbc/cppconn/driver.h>
+#include <mysql/include/jdbc/cppconn/exception.h>
+#include <mysql/include/jdbc/cppconn/resultset.h>
+#include <mysql/include/jdbc/cppconn/statement.h>
+#include <mysql/include/jdbc/cppconn/resultset.h>
+#include "databaseConnection.h"
+
+using namespace std;
+
+int main(){
+    databaseConnection dbConnect;
+    sql::Connection *con;
+    con = dbConnect.dbConnection();
+    return 0;
+}
